ee615e9743a683962c83e222eb43c51a4087472c
heaton <heaton@student.mamkschools.org>
2018-09-20 09:53:40 -0400

    Commit 2

--- src/Ch7Arrays/WeatherAnalysis.java
+++ src/Ch7Arrays/WeatherAnalysis.java
@@ -6,10 +6,36 @@ import java.util.Scanner;
 
 public class WeatherAnalysis {
     public static void main(String[] args) {
-        //Step 1: Ask for number of days
         Scanner console = new Scanner(System.in);
+        int days = 0;
+        intro(console,days);
+
+
+        //Step 1: Ask for number of days
+    }
+    public static void intro(Scanner console, int days) { // Asks for user input # of days
         System.out.println("How many days?");
-            int days = console.nextInt();
+        days = console.nextInt();
+        int[] data = new int[days];
+        userInput(console, days, data);
+    }
+    public static void userInput(Scanner console, int days, int[] data) { //Asks for the high temperatures for each day
+        for (int i =0; i<days; i++) {
+            System.out.println("Day "+(i+1)+"'s high temp: ");
+            int temp = console.nextInt();
+            data[i] = temp;
+        }
+        averages(data, days);
+    }
+    public static void averages(int[] data, int days) { //Calculates and prints the average
+        int total =0;
+        double average;
+        for (int x =0; x<days; x++) {
+            total+=data[x];
+        }
+        average =(double)total/(double)days;
+        average = (Math.round(average*10.0))/10.0;
+        System.out.println("Average temp = "+average);
     }
 }
 //Solution:
