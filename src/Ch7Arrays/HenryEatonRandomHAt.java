1286f16eb4f58a75e7edd1bf70f3a6c34e03b3a8
hheaton <43209573+hheaton@users.noreply.github.com>
2018-10-11 10:18:34 -0400

    Update HenryEatonRandomHAt.java

--- src/Ch7Arrays/HenryEatonRandomHAt.java
+++ src/Ch7Arrays/HenryEatonRandomHAt.java
@@ -1,36 +1,42 @@
-package Ch7Arrays;
+// This program generates a bracket for n number of teams entered by the user.
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class HenryEatonRandomHAt {
+public class HenryEatonRandomHat {
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
         System.out.print("How many teams? PLease enter an even number: ");
         int teams = console.nextInt();
         while(teams%2 == 1) {
             System.out.print("How many teams? PLease enter an even number: ");
-             teams = console.nextInt();
+            teams = console.nextInt();
         }
         int[] array = new int[teams];
         calculate(array, teams);
 
     }
+    // This method fills the array with a random number from 1 to the number of teams entered by the user
     public static void calculate(int[] array, int teams) {
         int j =0;
+        int x = 1;
+        for (int i = 0; i<=array.length-1; i++) {
+            j = (int) (Math.random() * teams);
+            if (array[j] == 0) {  // Checks if the value assigned to the randomly chosen index equals zero
+                array[j] = x; //Populates random index with value 1 - 10;
+                x++;
+            } else {
+                i--;
 
-        for (int i = 1; i<=teams; i++) {
-            int rand = (int)(Math.random()*teams +1);
-            for (int x = 0; x < i; x++) {
-                if (array[x] == rand) {
-                    rand = (int)(Math.random()*teams +1);
-                }
             }
-            array[j] = rand;
-            j++;
         }
-        System.out.println(Arrays.toString(array));
-    }
+        printBracket(array, teams);
     }
 
-
+    // this method prints the bracket for the teams using a for loop
+    public static void printBracket(int[] array, int teams) {
+        for (int i=0; i<teams; i+=2) {
+            System.out.println(array[i] + " vs. " + array[i+1]);
+        }
+    }
+}
